34213595548529255896a71126c9b123fa57837f
Seth Sutherland <cokolatepal@gmail.com>
2017-05-18 09:30:09 -0600

    finished

--- script.js
+++ script.js
@@ -1,134 +1,125 @@
+var margin = {top: 20, right: 40, bottom: 30, left: 20},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom,
+    barWidth = Math.floor(width / 19) - 1;
 
+var x = d3.scale.linear()
+    .range([barWidth / 2, width - barWidth / 2]);
 
-var margin = {
-    top: 20,
-    right: 20,
-    bottom: 30,
-    left: 40
-};
-var width = 960 - margin.left - margin.right;
-var height = 500 - margin.top - margin.bottom;
+var y = d3.scale.linear()
+    .range([height, 0]);
 
-var formatPercent = d3.format(".0%");
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("right")
+    .tickSize(-width)
+    .tickFormat(function(d) { return Math.round(d / 1e6) + "M"; });
 
-var x = d3.scale.ordinal().rangeRoundBands([0, width], .1, 1);
-
-var y = d3.scale.linear().range([height, 0]);
-
-var xAxis = d3.svg.axis().scale(x).orient("bottom");
-
-var yAxis = d3.svg.axis().scale(y).orient("left").tickFormat(formatPercent);
-
-var svg = d3.select("body").append(svg)
+// An SVG element with a bottom-right origin.
+var svg = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.json("./data.json", function(error, data) {
+// A sliding container to hold the bars by birthyear.
+var birthyears = svg.append("g")
+    .attr("class", "birthyears");
 
-    data.forEach(function (d) {
-        d.frequency = +d.frequency;
+// A label for the current year.
+var title = svg.append("text")
+    .attr("class", "title")
+    .attr("dy", ".71em")
+    .text(2000);
+
+d3.csv("population.csv", function(error, data) {
+
+    // Convert strings to numbers.
+    data.forEach(function(d) {
+        d.people = +d.people;
+        d.year = +d.year;
+        d.age = +d.age;
     });
 
-    x.domain(data.map(function (d) {
-        return d.letter;
-    }));
+    // Compute the extent of the data set in age and years.
+    var age1 = d3.max(data, function(d) { return d.age; }),
+        year0 = d3.min(data, function(d) { return d.year; }),
+        year1 = d3.max(data, function(d) { return d.year; }),
+        year = year1;
 
-    y.domain([0, d3.max(data, function (d) {
-        return d.frequency;
-    })]);
+    // Update the scale domains.
+    x.domain([year1 - age1, year1]);
+    y.domain([0, d3.max(data, function(d) { return d.people; })]);
 
-    svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
+    // Produce a map from year and birthyear to [male, female].
+    data = d3.nest()
+        .key(function(d) { return d.year; })
+        .key(function(d) { return d.year - d.age; })
+        .rollup(function(v) { return v.map(function(d) { return d.people; }); })
+        .map(data);
 
+    // Add an axis to show the population values.
     svg.append("g")
         .attr("class", "y axis")
+        .attr("transform", "translate(" + width + ",0)")
         .call(yAxis)
-        .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", ".71em")
-        .style("text-anchor", "end")
-        .text("Frequency");
+        .selectAll("g")
+        .filter(function(value) { return !value; })
+        .classed("zero", true);
 
-    svg.selectAll(".bar")
-        .data(data)
+    // Add labeled rects for each birthyear (so that no enter or exit is required).
+    var birthyear = birthyears.selectAll(".birthyear")
+        .data(d3.range(year0 - age1, year1 + 1, 5))
+        .enter().append("g")
+        .attr("class", "birthyear")
+        .attr("transform", function(birthyear) { return "translate(" + x(birthyear) + ",0)"; });
+
+    birthyear.selectAll("rect")
+        .data(function(birthyear) { return data[year][birthyear] || [0, 0]; })
         .enter().append("rect")
-        .attr("class", "bar")
-        .attr("x", function (d) {
-            return x(d.letter);
-        })
-        .attr("width", x.rangeBand())
-        .attr("y", function (d) {
-            return y(d.frequency);
-        })
-        .attr("height", function (d) {
-            return height - y(d.frequency);
-        });
+        .attr("x", -barWidth / 2)
+        .attr("width", barWidth)
+        .attr("y", y)
+        .attr("height", function(value) { return height - y(value); });
 
-    d3.select("input").on("change", change);
+    // Add labels to show birthyear.
+    birthyear.append("text")
+        .attr("y", height - 4)
+        .text(function(birthyear) { return birthyear; });
 
-    var sortTimeout = setTimeout(function () {
-        d3.select("input").property("checked", true).each(change)
-    }, 2000);
+    // Add labels to show age (separate; not animated).
+    svg.selectAll(".age")
+        .data(d3.range(0, age1 + 1, 5))
+        .enter().append("text")
+        .attr("class", "age")
+        .attr("x", function(age) { return x(year - age); })
+        .attr("y", height + 4)
+        .attr("dy", ".71em")
+        .text(function(age) { return age; });
 
-    function change() {
-        clearTimeout(sortTimeout);
+    // Allow the arrow keys to change the displayed year.
+    window.focus();
+    d3.select(window).on("keydown", function() {
+        switch (d3.event.keyCode) {
+            case 37: year = Math.max(year0, year - 10); break;
+            case 39: year = Math.min(year1, year + 10); break;
+        }
+        update();
+    });
 
-        var x0 = x.domain(data.sort(this.checked
-            ? function (a, b) {
-                return b.frequency - a.frequency;
-            }
-            : function (a, b) {
-                return d3.ascending(a.letter, b.letter);
-            }))
-            .map(function (d) {
-                return d.letter;
-            })
-            .copy();
+    function update() {
+        if (!(year in data)) return;
+        title.text(year);
 
-        svg.selectAll(".bar")
-            .sort(function (a, b) {
-                return x0(a.letter) - x0(b.letter);
-            });
+        birthyears.transition()
+            .duration(750)
+            .attr("transform", "translate(" + (x(year1) - x(year)) + ",0)");
 
-        var transition = svg.transition().duration(750),
-            delay = function (d, i) {
-                return i * 50;
-            };
-
-        transition.selectAll(".bar")
-            .delay(delay)
-            .attr("x", function (d) {
-                return x0(d.letter);
-            });
-
-        transition.select(".x.axis")
-            .call(xAxis)
-            .selectAll("g")
-            .delay(delay);
+        birthyear.selectAll("rect")
+            .data(function(birthyear) { return data[year][birthyear] || [0, 0]; })
+            .transition()
+            .duration(750)
+            .attr("y", y)
+            .attr("height", function(value) { return height - y(value); });
     }
-
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
